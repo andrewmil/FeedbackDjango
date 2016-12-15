bd53419dfcbec6e22604a94bbfb5bc5bea725444
Andrew Millar <millaa@C02SQ03AG8WL.local>
2016-12-15 13:39:17 +0000

    updating form validation

--- polls/forms.py
+++ polls/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 class SurveyFeedback(forms.Form):
     CHOICES = [('very satisfied', 'very satisfied'), ('satisfied', 'satisfied'), ('neither', 'neither'), ('dissatisfied', 'dissatisfied'), ('very dissatisfied', 'very dissatisfied')]
-    radioFeedback = forms.ChoiceField(label='How satisfied were you with the service you just received?', choices=CHOICES, widget=forms.RadioSelect())
-    textFeedback = forms.CharField(label='Survey Feedback', max_length=1200)
+    radioFeedback = forms.ChoiceField(label='How satisfied were you with the service you just received?', choices=CHOICES, widget=forms.RadioSelect(), required=True)
+    textFeedback = forms.CharField(label='Survey Feedback', max_length=1200, required=False)
