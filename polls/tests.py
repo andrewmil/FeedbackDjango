9acbe26163704283da30f6270f400e9796686f8d
Gemma Irving <irving@C02SQ035G8WL.local>
2016-12-16 13:56:19 +0000

    unit tests currently includes db test

--- polls/tests.py
+++ polls/tests.py
@@ -1,8 +1,19 @@
 from django.test import TestCase
 from polls.models import Feedback
+from django.db import connections
+from polls.forms import SurveyFeedback
 
 # Create your tests here.
 
 class FeedbackTestCase(TestCase):
     def test_dbconnect(self):
+        db_conn = connections['default']
+        try:
+            c = db_conn.cursor()
+        except OperationalError:
+            connected = False
+        else:
+            connected = True
+
+    def test_formValidation(self):
         
