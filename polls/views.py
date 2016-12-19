0c230e4492defda67028c0de594fc3cfadfa1f0e
Andrew Millar <millaa@C02SQ03AG8WL.local>
2016-12-19 09:02:52 +0000

    changing database ip

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
-from django.template import loader
 from .forms import SurveyFeedback
 from .models import Feedback
-from .static.polls.scripts.py.dbConnect import *
-from .static.polls.scripts.py.validation import *
+# from .static.polls.scripts.py.dbConnect import *
+# from .static.polls.scripts.py.validation import *
 # Create your views here.
 
 def index(request):
@@ -17,7 +16,7 @@ def database_Send(request):
 
     if request.method == 'POST':
         form = SurveyFeedback(request.POST)
-        if form.is_valid(): # All validation rules pass
+        if form.is_valid(): ## All validation rules pass
             ## retrieving form data ##
             satisfaction = form.cleaned_data['radioFeedback']
             feedback = form.cleaned_data['textFeedback']
@@ -30,9 +29,8 @@ def database_Send(request):
     f.save()
 
     ## validation ##
-    if (feedbackValidation(feedback) or satisfactionValidation(satisfaction)):
-         return redirect('/polls/')
+    # if (feedbackValidation(feedback) or satisfactionValidation(satisfaction)):
+    #      return redirect('/polls/')
 
     print("done")
-
-    return HttpResponseRedirect('/polls/')
+    return render(request, 'polls/confirm.html')
