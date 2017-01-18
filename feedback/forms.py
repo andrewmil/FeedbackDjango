e027379635a3be2ee8d0571f34dba2f43285aaa5
Andrew Millar <millaa@C02SQ03AG8WL.local>
2017-01-18 14:59:41 +0000

    removing required answer for satisfaction

--- feedback/forms.py
+++ feedback/forms.py
@@ -3,5 +3,5 @@ from django import forms
 
 class SurveyFeedback(forms.Form):
     CHOICES = [('very satisfied', 'very satisfied'), ('satisfied', 'satisfied'), ('neither', 'neither'), ('dissatisfied', 'dissatisfied'), ('very dissatisfied', 'very dissatisfied')]
-    radioFeedback = forms.ChoiceField(label='How satisfied were you with the service you just received?', choices=CHOICES, widget=forms.RadioSelect(), required=True)
+    radioFeedback = forms.ChoiceField(label='How satisfied were you with the service you just received?', choices=CHOICES, widget=forms.RadioSelect(), required=False)
     textFeedback = forms.CharField(label='Survey Feedback', max_length=1200, required=False, widget=forms.Textarea(attrs={'rows': 10, 'cols': 80, 'onKeyDown':'return setTimeout(remainingChars, 10);'}))
