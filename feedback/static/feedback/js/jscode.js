c9bc108ab3e22e203639367f795c57a2216f87a5
Gemma Irving <irving@C02SQ035G8WL.local>
2017-01-18 13:33:12 +0000

    add character limit reach text

--- feedback/static/feedback/js/jscode.js
+++ feedback/static/feedback/js/jscode.js
@@ -1,5 +1,11 @@
 function remainingChars() {
     var text = document.getElementById("id_textFeedback").value;
     var chars = text.length;
-    document.getElementById("chars").innerHTML = 'Characters remaining: ' + (1200 - chars);
+    if(chars < 1200){
+      document.getElementById("chars").innerHTML = 'Characters remaining: ' + (1200 - chars);
+    }
+    else {
+      document.getElementById("chars").innerHTML = 'Character limit reached!';
+      document.getElementById("chars").style.color = 'red';
+    }
 }
