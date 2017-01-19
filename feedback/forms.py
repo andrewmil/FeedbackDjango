53b05e2beb7be0f62968da1cd9ced99289cf8242
MIslam92 <Mohammad.Islam@qa.com>
2017-01-19 09:41:23 +0000

    Small update to index.html

--- feedback/forms.py
+++ feedback/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 
 class SurveyFeedback(forms.Form):
-    CHOICES = [('very satisfied', 'very satisfied'), ('satisfied', 'satisfied'), ('neither', 'neither'), ('dissatisfied', 'dissatisfied'), ('very dissatisfied', 'very dissatisfied')]
+    CHOICES = [('Very Satisfied', 'Very Satisfied'), ('Satisfied', 'Satisfied'), ('Neither', 'Neither'), ('Dissatisfied', 'Dissatisfied'), ('Very Dissatisfied', 'Very Dissatisfied')]
     radioFeedback = forms.ChoiceField(label='How satisfied were you with the service you just received?', choices=CHOICES, widget=forms.RadioSelect(), required=False)
     textFeedback = forms.CharField(label='Survey Feedback', max_length=1200, required=False, widget=forms.Textarea(attrs={'rows': 10, 'cols': 80, 'onKeyDown':'return setTimeout(remainingChars, 10);'}))
